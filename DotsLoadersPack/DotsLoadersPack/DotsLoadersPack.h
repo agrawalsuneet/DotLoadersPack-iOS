6ff6b970bf7e0239ce4e344f587010a95794fffb
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-03 10:23:55 +0000

    added project

---
+++ DotsLoadersPack/DotsLoadersPack/DotsLoadersPack.h
@@ -0,0 +1,19 @@
+//
+//  DotsLoadersPack.h
+//  DotsLoadersPack
+//
+//  Created by Suneet on 11/24/17.
+//  Copyright © 2017 agrawalsuneet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DotsLoadersPack.
+FOUNDATION_EXPORT double DotsLoadersPackVersionNumber;
+
+//! Project version string for DotsLoadersPack.
+FOUNDATION_EXPORT const unsigned char DotsLoadersPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotsLoadersPack/PublicHeader.h>
+
+
